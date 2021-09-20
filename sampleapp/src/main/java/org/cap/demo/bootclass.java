58e60996cb68011deec376fa07aaaa9298a3b437
sindurgoud <sindu.rudragowni@gmail.com>
2021-09-20 11:02:55 +0530

    show method added

--- sampleapp/src/main/java/org/cap/demo/bootclass.java
+++ sampleapp/src/main/java/org/cap/demo/bootclass.java
@@ -4,7 +4,11 @@ public class bootclass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from STS");
-
+		show();
+    }
+    
+	public static void show() {
+		System.out.println("Show method here!");
 	}
-
+	
 }
